3c36c52360547c6bcd17d20dc094442d3f572f97
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2025-12-14 05:22:50 +0530

    log store setup

---
+++ src/main/java/com/projects/logstore/LogStoreApplication.java
@@ -0,0 +1,13 @@
+package com.projects.logstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogStoreApplication.class, args);
+    }
+
+}
